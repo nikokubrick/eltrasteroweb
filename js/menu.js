e68185dde019ae9d48b95c2694ff88b62c6570dc
Nicolas Saavedra <saavedranicolas1982@gmail.com>
2019-02-04 15:25:08 -0300

    subida de archivos

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+let btnMenu = document.getElementById('btnmenu');
+let menu = document.getElementById('menu');
+btnMenu.addEventListener('click', function(){
+    'use strict';
+    menu.classList.toggle('mostrar');
+});
